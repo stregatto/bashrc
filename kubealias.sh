dda85feb6ceeae10aab9d95c1273a59e6d80ec81
stregatto <emmecerre@gmail.com>
2020-03-31 10:15:09 +0200

    add single config file management

--- kubealias.sh
+++ kubealias.sh
@@ -3,14 +3,19 @@
 #
 # my kubernetes alias
 #
+# you can use both standard kubernetes config file or ${context}.yaml file 
+#
 
 GREPPODSTATUS='Error|Unknown|CrashLoopBackOff|Terminating|Creating|Pending|ErrImagePull|ImagePullBackOff'
 
+# list of contexts
+# put a ${context}.yaml file if you want to separate config files
 KENV__sre="sre"
 KENV__nonprd="nonprod"
 KENV__dev="dev"
 KENV__prd="production"
 
+# aliases 
 ALIAS__node="get node --no-headers |cut -d\" \" -f1"
 # to be fixed
 ALIAS__wwp="get pod --all-namespaces -o wide|egrep '${GREPPODSTATUS}'"
@@ -21,12 +26,22 @@ ALIAS__uncordon="uncordon"
 function build_alias() {
   keyContext=$1
   CONTEXT=$2
-  alias k${keyContext}="kubectl --context ${CONTEXT}"
+  if [ -f "${HOME}/.kube/${CONTEXT}.yaml" ];
+    then
+        alias k${keyContext}="kubectl  --kubeconfig=${HOME}/.kube/${CONTEXT}.yaml"
+    else
+        alias k${keyContext}="kubectl --context ${CONTEXT}"
+  fi
   for k in ${!ALIAS__*}; do
     key=$k
     value=${!key}
     key=${key##ALIAS__}
-    alias k${keyContext}${key}="kubectl --context ${CONTEXT} ${value}"
+    if [ -f "${HOME}/.kube/${CONTEXT}.yaml" ];
+    then
+        alias k${keyContext}${key}="kubectl  --kubeconfig=${HOME}/.kube/${CONTEXT}.yaml ${value}"
+    else
+        alias k${keyContext}${key}="kubectl --context ${CONTEXT} ${value}"
+    fi
   done
 }
 
